7af802ca83ce65762a2b15849b81822b45c75d76
Lachlan McCalman <lachlan@mccalman.info>
2022-03-28 20:13:59 +1100

    frontend talking to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { Button } from 'react-bootstrap';
 
 import './App.css';
+
+import { MainPane } from './Main';
 
 function App() {
   return (
     <RecoilRoot>
     <div className="App">
-      <h1 className="text-3xl font-bold underline"> Predictions 2022 </h1>
-      <h2> Website by lachy and Jac </h2>
-      <Button />
+      <MainPane />
     </div>
     </RecoilRoot>
   );
